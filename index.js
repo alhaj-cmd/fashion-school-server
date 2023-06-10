83bafd641342683728f0ebb35f17fb38d0b49601
github_username <you@domain.com>
2023-06-10 16:51:51 +0600

    user get

--- index.js
+++ index.js
@@ -33,6 +33,13 @@ async function run() {
 
 
     // user api
+
+    app.get('/user', async(req, res ) =>{
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
+
     app.post('/users', async(req, res) =>{
       const user =  req.body;
       const query = {email: user.email}
